b9b23a97f6f69e030eb034df9f264cf55e955e18
chrispbooth <chris.p.booth@gmail.com>
2017-11-06 07:23:59 -0500

    clean

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+ext_modules = [
+    Extension(
+        "simpSent",
+        ["simpSent.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp'],
+    )
+]
 
 setup(
-    ext_modules=cythonize("cySent.pyx"),
+    name='simple-sentiment',
+    ext_modules=cythonize(ext_modules),
 )
