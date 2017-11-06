4577cf7efc689843545ca7f900f883ed5a76147e
chrispbooth <chris.p.booth@gmail.com>
2017-11-05 21:23:52 -0500

    9

--- cySent.pyx
+++ cySent.pyx
@@ -5,7 +5,7 @@ from tweepy import OAuthHandler
 from textblob import TextBlob
 from flask import Flask
 cimport Cython
-from Cython.parallel cimport prange
+from Cython.parallel import prange
 #import numpy
 #from numpy cimport ndarray as ar
 cimport openmp
